e827aebae86722d512c769ddca15d76f4cc4b140
Patrik Radl <patrik.radl@icloud.com>
2025-04-07 13:07:33 +0200

    message

--- searching.py
+++ searching.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -11,8 +11,15 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
-    file_path = os.path.join(cwd_path, file_name)
 
+    file_path = os.path.join(cwd_path, file_name)
+    with open("sequential.json", "r") as file_obj:
+        data = json.load(file_obj)
+    if field in data.keys():
+        return data[field]
+    else:
+        print(f"Field {field} not exist")
+        return None
 
 def main():
     pass
@@ -20,3 +27,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+    json_filename = "sequential.json"
+    sequential_data = read_data(json_filename,"unordered_numbers")
+    print(sequential_data)
